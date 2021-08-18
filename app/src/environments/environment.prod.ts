f251c8c89682abee2f3fab671902935e96a48fb2
Hasan <hasanaburayyan21@gmail.com>
2021-08-17 22:46:54 -0400

    changed URL

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   cognitoUserPoolId: "us-east-1_trlXTqkgW",
   cognitoAppClientId: "3g4vo0pf3q40uq2tijvif8is5k",
-  awsGloryRestApi: "https://umkdiusrei.execute-api.us-east-1.amazonaws.com/prod/"
+  awsGloryRestApi: "https://s1omawaz6g.execute-api.us-east-1.amazonaws.com/prod/"
 };
